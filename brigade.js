f5edc5651c6503e361ead77e2ea5af3bd2216c21
Ralph Squillace <ralph@squillace.com>
2017-12-01 23:25:33 -0800

    more logging

--- brigade.js
+++ brigade.js
@@ -19,7 +19,7 @@ events.on("push", (brigadeEvent, project) => {
 
 
   var m = `Github ${brigadeEvent.type} event on the \"${brigConfig.get("branch")}\" branch.\nCommit link: ${gitPayload.head_commit.url}.\nCommitted by ${gitPayload.head_commit.author.username} (${gitPayload.head_commit.author.email}).`
-
+  console.log(m)
   if (project.secrets.SLACK_WEBHOOK) {
     var container = new Job("slack-notify")
 
