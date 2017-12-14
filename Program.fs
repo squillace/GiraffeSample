c0c0b958c6b704888b7b255afd08b9f3619fc1b0
Ralph Squillace <ralph@squillace.com>
2017-12-13 22:28:22 -0800

    cleaning

--- Program.fs
+++ Program.fs
@@ -46,7 +46,7 @@ let webApp =
 // ---------------------------------
 
 let errorHandler (ex : Exception) (logger : ILogger) =
-    logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
+    logger.LogError(EventId(), ex, "An unhandled exception occurred while executing the request.")
     clearResponse >=> setStatusCode 500 >=> text ex.Message
 
 // ---------------------------------
